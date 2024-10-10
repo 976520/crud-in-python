9f99215af459bdd3b8f2389f63925e2524925896
송재욱 <123460320+976520@users.noreply.github.com>
2024-10-10 18:13:05 +0900

    app

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import Form from "../components/Form/Form";
+import BoardTable from "../components/Table/BoardTable";
+
+const Container = styled.div`
+  text-align: center;
+  background: #e0e0e0;
+  border-radius: 20px;
+  box-shadow: 8px 8px 16px #b0b0b0, -8px -8px 16px #ffffff;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  color: #333;
+  text-shadow: 1px 1px 2px #fff;
+`;
+
+function Home() {
+  const [board, setBoard] = useState([]);
+  const [title, setTitle] = useState("");
+  const [context, setContext] = useState("");
+
+  useEffect(() => {
+    fetchBoard();
+  }, []);
+
+  const fetchBoard = async () => {
+    const response = await axios.get("http://localhost:5000/api/board");
+    setBoard(response.data);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:5000/api/add", { title, context });
+    setTitle("");
+    setContext("");
+    fetchBoard();
+  };
+
+  return (
+    <Container>
+      <Title>나도 이제 백엔드?</Title>
+      <h4>add</h4>
+      <Form title={title} setTitle={setTitle} context={context} setContext={setContext} handleSubmit={handleSubmit} />
+      <h4>view</h4>
+      <BoardTable board={board} />
+    </Container>
+  );
+}
+
+export default Home;
