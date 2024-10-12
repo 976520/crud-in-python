a12b15d1d88cd961640a36dcecc5defcf36a4a1c
송재욱 <123460320+976520@users.noreply.github.com>
2024-10-13 08:41:35 +0900

    i hate exam

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -6,8 +6,18 @@ import Input from "../components/Form/Input";
 const API_URL = "http://localhost:3002/api";
 
 function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,12 +27,12 @@ function SignIn() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify(formData),
       });
       const data = await response.json();
       if (data.success) {
         alert("로그인 성공");
-        localStorage.setItem("user-id", email);
+        localStorage.setItem("user-id", formData.email);
         window.location.href = "/home";
       } else {
         alert("로그인 실패");
@@ -38,10 +48,10 @@ function SignIn() {
       <Title>로그인</Title>
       <form onSubmit={handleSubmit}>
         <p>email</p>
-        <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <Input type="email" name="email" value={formData.email} onChange={handleChange} required />
         <br />
         <p>password</p>
-        <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <Input type="password" name="password" value={formData.password} onChange={handleChange} required />
         <br />
         <Input id="submit" type="submit" value="로그인" />
       </form>
