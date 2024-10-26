69be1e434ca9d3f332dc520e8941cde4b3472f56
송재욱 <haensol976520@gmail.com>
2024-10-27 00:24:50 +0900

    handleSubmit

--- client/src/components/Form/BoardForm.tsx
+++ client/src/components/Form/BoardForm.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import TextArea from "./TextArea.tsx";
 import Input from "./Input.tsx";
 
-const BoardForm = ({ title, setTitle, context, setContext, handleSubmit }) => (
-  <form onSubmit={handleSubmit}>
-    <p>title</p>
-    <Input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-    <p>context</p>
-    <TextArea value={context} onChange={(e) => setContext(e.target.value)}></TextArea>
-    <br />
-    <Input type="submit" value="submit" id="submit" />
-  </form>
-);
+const BoardForm = ({ title, setTitle, context, setContext, handleSubmit }) => {
+  const isDisabled = !title.trim() || !context.trim();
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>title</p>
+      <Input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <p>context</p>
+      <TextArea value={context} onChange={(e) => setContext(e.target.value)}></TextArea>
+      <br />
+      <Input type="submit" value="submit" id="submit" disabled={isDisabled} />
+    </form>
+  );
+};
 
 export default BoardForm;
