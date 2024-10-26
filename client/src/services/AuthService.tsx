783d52f4a8f19ebef8cabb54e858061a7f142327
송재욱 <haensol976520@gmail.com>
2024-10-26 23:25:55 +0900

    ts

--- client/src/services/AuthService.tsx
+++ client/src/services/AuthService.tsx
@@ -4,11 +4,9 @@ const API_URL = "http://localhost:5000/api";
 
 export const authService = async (formData, service) => {
   const response = await axios.post(`${API_URL}/${service}`, formData, {
-    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(formData),
   });
   return response.data;
 };
